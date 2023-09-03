f47bd582cdaf767de746c0b945fa837018fddd91
Sławomir Urbaniec <72451564+Siiwson@users.noreply.github.com>
2023-09-03 12:01:11 +0200

    Delete shadow

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -83,15 +83,6 @@ const styles = StyleSheet.create({
   inputContainer: {
     alignItems: "center",
     backgroundColor: "#ededed",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 4,
-    zIndex: 5,
   },
   taskContainer: {
     flexDirection: "row",
